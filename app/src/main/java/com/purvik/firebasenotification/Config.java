6dce6c1f28cd7090c17e5da85895311986bbd82e
Purvik Rana <purvik1239@gmail.com>
2016-07-01 13:36:41 +0530

    FireBase RealTime Database Writing

---
+++ app/src/main/java/com/purvik/firebasenotification/Config.java
@@ -0,0 +1,11 @@
+package com.purvik.firebasenotification;
+
+/**
+ * Created by Purvik Rana on 16-06-2016.
+ *
+ */
+public class Config {
+
+    public static final String FIREBASE_URL = "https://fir-datawriting.firebaseio.com/";
+
+}
---
+++ app/src/main/java/com/purvik/firebasenotification/Person.java
@@ -0,0 +1,31 @@
+package com.purvik.firebasenotification;
+
+/**
+ * Created by Purvik Rana on 16-06-2016.
+ */
+public class Person {
+
+    //name and address string
+    private String name;
+    private String address;
+
+    public Person() {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
